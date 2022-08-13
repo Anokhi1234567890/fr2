0f101d563c129e53fa67faff9a90c372f5df4c16
Anokhi1234567890 <103378707+Anokhi1234567890@users.noreply.github.com>
2022-08-13 13:07:39 -0500

    Add files via upload

---
+++ New folder (2)/main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:100
+});
+
+
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_image' src = '"+data_uri+"'/>"
+    });
+}
+
+    console.log('ml5 version: ',ml5.version);
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yBeiJpKcR/.json',modelLoaded);
